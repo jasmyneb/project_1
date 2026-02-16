68fa0b0af70e325f00abb88d61ccbe2b34829efa
Jasmyne Brown <brownjasmyne07@gmail.com>
2026-02-16 11:15:05 -0500

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Jasmyne Brown on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
